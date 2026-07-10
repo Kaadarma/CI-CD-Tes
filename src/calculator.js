e17d7fd2bef465c486dcc6e3cdbb09e538741ef1
Eka Darma <putukaa378@gmail.com>
2026-07-10 11:11:49 +0800

    fix update

--- src/calculator.js
+++ src/calculator.js
@@ -4,17 +4,17 @@ function add(a, b) {
 
 function subtract(a, b) {
   return a - b;
+}
 
+function multiply(a, b) {
+  return a * b;
+}
 
-  function multiply(a, b) {
-    return a * b;
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error("Division by zero is not allowed");
   }
+  return a / b;
+}
 
-  function divide(a, b) {
-    if (b === 0) {
-      throw new Error("Division by zero is not allowed");
-    }
-    return a / b;
-  }
-
-  module.exports = { add, subtract, multiply, divide };
+module.exports = { add, subtract, multiply, divide };
